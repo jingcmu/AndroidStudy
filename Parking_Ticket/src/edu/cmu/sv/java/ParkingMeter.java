138d10e1266f685f74b363dabfc2d5ed3e3df0fb
Jacky Chen <jingchen@cmu.edu>
2013-12-14 17:03:55 -0800

    parking ticket project

---
+++ Parking_Ticket/src/edu/cmu/sv/java/ParkingMeter.java
@@ -0,0 +1,22 @@
+package edu.cmu.sv.java;
+
+/**
+ * @brief simulate a parking meter
+ * To know the number of minutes of parking time that has been purchased
+ */
+public class ParkingMeter {
+	private int purchasedMinutes;
+	
+	public ParkingMeter(int purchasedMinutes) {
+		setPurchasedMinutes(purchasedMinutes);
+	}
+	
+	//getter and setter
+	public int getPurchasedMinutes() {
+		return purchasedMinutes;
+	}
+	
+	public void setPurchasedMinutes(int purchasedMinutes) {
+		this.purchasedMinutes = purchasedMinutes;
+	}
+}
